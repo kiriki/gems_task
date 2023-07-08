d586719252e9bed23d793d5e4273de2ef0da3060
Kirill Che <kiriciri@gmail.com>
2023-07-08 18:28:52 +0300

    models

--- gems/models.py
+++ gems/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class GemItem(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Customer(models.Model):
+    username = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Deal(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    item = models.ForeignKey(GemItem, on_delete=models.CASCADE)
+    total = models.PositiveIntegerField()
+    quantity = models.PositiveSmallIntegerField()
+    date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return str(self.id)
