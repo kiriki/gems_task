432e95a0bc82211d8c2371279c167d00f94c9851
Kirill Che <kiriciri@gmail.com>
2023-07-08 21:59:20 +0300

    test api entrypoints

--- gems/tests.py
+++ gems/tests.py
@@ -1,3 +1,68 @@
-from django.test import TestCase
+from pathlib import Path
 
-# Create your tests here.
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from django.urls import reverse
+
+from gems.urls import DEALS_VIEW_NAME
+
+deals_path = reverse(DEALS_VIEW_NAME)
+cwd = Path.cwd()
+
+CUSTOMERS_COUNT_EMPTY = 0
+CUSTOMERS_COUNT_FILLED = 5
+
+
+@pytest.fixture
+def client() -> APIClient:
+    return APIClient()
+
+
+@pytest.fixture
+def client_data_filled(csv_file: Path, client: APIClient) -> APIClient:
+    with csv_file.open('rb') as source_file:
+        data = {'deals': source_file}
+        client.post(deals_path, data, format='multipart')
+    return client
+
+
+@pytest.fixture
+def csv_file() -> Path:
+    return cwd / 'csv_data/deals.csv'
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    ('client_name', 'customers_count'),
+    [
+        ('client', CUSTOMERS_COUNT_EMPTY),
+        ('client_data_filled', CUSTOMERS_COUNT_FILLED),
+    ],
+)
+def test_customers_empty(
+    client_name: str, customers_count: int, request: pytest.FixtureRequest
+):
+    client = request.getfixturevalue(client_name)
+    response = client.get(deals_path, format='json')
+    assert response.status_code == status.HTTP_200_OK, response.content
+
+    result = response.json()
+
+    assert 'response' in result
+    assert len(result['response']) == customers_count
+
+
+@pytest.mark.django_db
+def test_upload_deals(csv_file: Path, client: APIClient):
+    with csv_file.open('rb') as source_file:
+        data = {'deals': source_file}
+        response = client.post(deals_path, data, format='multipart')
+
+    assert response.status_code == status.HTTP_200_OK
+
+    result = response.json()
+
+    assert 'Status' in result
+    assert result['Status'] == 'OK'
--- gems/urls.py
+++ gems/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from gems import views
 
+DEALS_VIEW_NAME = 'deals'
+
 urlpatterns = [
-    path('', views.DealsStat.as_view(), name='deals'),
+    path('', views.DealsStat.as_view(), name=DEALS_VIEW_NAME),
 ]
